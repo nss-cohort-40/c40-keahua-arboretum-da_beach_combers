9ffa33eef89720b6b8f7ae814948707947c3d6c8
Roxanne Nasraty <rsnasraty@gmail.com>
2020-07-23 14:35:12 -0500

    Added Plant classes

--- plants/plant.py
+++ plants/plant.py
@@ -7,3 +7,85 @@ class Plant:
       self.sunlight = sunlight
       self.seeds = seeds
       self.insecticide = insecticide
+
+    def plant(self):
+        print(f'Plant in {environment}')
+
+    def add_plant(self, plant):
+        self.animals.append(plant)
+
+    def remove_plant(self, plant):
+
+
+class Mountain_Apple_Tree(Plant):
+
+    # Remove redundant properties from Llama's initialization, and set their values via Animal
+    def __init__(self, name, location, species, sunlight, seeds, insecticide):
+        super().__init__(name, species, sunlight, seeds, insecticide)
+        self.species = species
+        self.sunlight = sunlight
+        self.seeds = seeds
+        self.insecticide = insecticide
+
+    def plant(self):
+        print(f'Plant in {environment}')
+
+    def add_plant(self, plant):
+        self.animals.append(plant)
+
+    def remove_plant(self, plant):
+
+
+class Silversword(Plant):
+
+    # Remove redundant properties from Llama's initialization, and set their values via Animal
+    def __init__(self, name, location, species, sunlight, seeds, insecticide):
+        super().__init__(name, species, sunlight, seeds, insecticide)
+        self.species = species
+        self.sunlight = sunlight
+        self.seeds = seeds
+        self.insecticide = insecticide
+
+    def plant(self):
+        print(f'Plant in {environment}')
+     
+    def add_plant(self, plant):
+        self.animals.append(plant)
+
+    def remove_plant(self, plant):
+
+class Rainbow_Eucalyptus_Tree(Plant):
+
+    # Remove redundant properties from Llama's initialization, and set their values via Animal
+    def __init__(self, name, location, species, sunlight, seeds, insecticide):
+        super().__init__(name, species, sunlight, seeds, insecticide)
+        self.species = species
+        self.sunlight = sunlight
+        self.seeds = seeds
+        self.insecticide = insecticide
+
+    def plant(self):
+        print(f'Plant in {environment}')
+
+    def add_plant(self, plant):
+        self.animals.append(plant)
+
+    def remove_plant(self, plant):
+
+class Blue_Jade_Vine(Animal):
+
+    # Remove redundant properties from Llama's initialization, and set their values via Animal
+    def __init__(self, name, location, species, sunlight, seeds, insecticide):
+        super().__init__(name, species, sunlight, seeds, insecticide)
+        self.species = species
+        self.sunlight = sunlight
+        self.seeds = seeds
+        self.insecticide = insecticide
+
+    def plant(self):
+        print(f'Plant in {environment}')
+
+    def add_plant(self, plant):
+        self.animals.append(plant)
+
+    def remove_plant(self, plant):
