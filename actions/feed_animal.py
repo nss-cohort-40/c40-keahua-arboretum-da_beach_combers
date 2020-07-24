d68bf391c6462c85b7dafa373fb8e51ffd70c635
Stephen Castaneda <stephen.castaneda40@gmail.com>
2020-07-24 10:56:53 -0500

    wip

---
+++ actions/feed_animal.py
@@ -0,0 +1,43 @@
+import os
+from animals import RiverDolphin
+
+def feed_animal(arboretum):
+    os.system('cls' if os.name == 'nt' else 'clear')
+    print("1. Gold Dust Day Gecko")
+    print("2. Nene Goose")
+    print("3. Kīkākapu")
+    print("4. Ope'ape'a")
+    print("5. Pueo")
+    print("6. River dolphin")
+    print("7. Happy Face Spider")
+    print("8. Ulae")
+
+    choice = input("Choose animal to feed. > ")
+
+    if choice == "1":
+      pass
+
+    if choice == "2":
+      pass
+
+    if choice == "3":
+      pass
+
+    if choice == "4":
+      pass
+
+    if choice == "5":
+      pass
+
+    if choice == "6":
+
+      choice = input("What is on the menu for the River Dolphin today?")
+
+    if choice == "7":
+      pass
+
+    if choice == "8":
+      pass
+
+    
+
