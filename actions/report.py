39127fdd7e410881318aa2fba3ff6fc955ce18b6
Adrian Garmendia <dejima@gmail.com>
2020-07-24 14:34:50 -0500

    wip

--- actions/report.py
+++ actions/report.py
@@ -1,6 +1,7 @@
 def build_facility_report(arboretum):
     for river in arboretum.rivers:
         print(f'River [{river.id}]')
-        for plant in river.plants:
-            print(f'   {plant.species} [{plant.id}]')
+        for animal in river.animals:
+            print(f'   {animal.species} [{animal.id}]')
+
     input("\n\nPress any key to continue...")
