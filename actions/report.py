9327bce13a960f363b0beb559ae8bdda2de1d2f5
Roxanne Nasraty <rsnasraty@gmail.com>
2020-07-28 14:37:12 -0500

    Report id strings shortened

--- actions/report.py
+++ actions/report.py
@@ -2,37 +2,37 @@
 def build_facility_report(arboretum):
     # os.system('cls' if os.name == 'nt' else 'clear')
     for river in arboretum.rivers:
-        print(f'River [{river.id}] has {len(river.animals)} animals and {len(river.plants)} plants')
+        print(f'River [{str(river.id)[:8]}] has {len(river.animals)} animals and {len(river.plants)} plants')
         for animal in river.animals:
             print(f'      {animal.species} [{animal.id}]')
         for plant in river.plants:
             print(f'      {plant.species} [{plant.id}]')
     for coastline in arboretum.coastlines:
-        print(f'Coastline [{coastline.id}] has {len(coastline.animals)} animals and {len(coastline.plants)} plants')
+        print(f'Coastline [{str(coastline.id)[:8]}] has {len(coastline.animals)} animals and {len(coastline.plants)} plants')
         for animal in coastline.animals:
             print(f'      {animal.species} [{animal.id}]')
         for plant in coastline.plants:
             print(f'      {plant.species} [{plant.id}]')
     for forest in arboretum.forests:
-        print(f'Forest [{forest.id}] has {len(forest.animals)} animals and {len(forest.plants)} plants')
+        print(f'Forest [{str(forest.id)[:8]}] has {len(forest.animals)} animals and {len(forest.plants)} plants')
         for animal in forest.animals:
             print(f'      {animal.species} [{animal.id}]')
         for plant in forest.plants:
             print(f'      {plant.species} [{plant.id}]')
     for grassland in arboretum.grasslands:
-        print(f'Grassland [{grassland.id}] has {len(grassland.animals)} animals and {len(grassland.plants)} plants')
+        print(f'Grassland [{str(grassland.id)[:8]}] has {len(grassland.animals)} animals and {len(grassland.plants)} plants')
         for animal in grassland.animals:
             print(f'      {animal.species} [{animal.id}]')
         for plant in grassland.plants:
             print(f'      {plant.species} [{plant.id}]')
     for mountain in arboretum.mountains:
-        print(f'Mountain [{mountain.id}] has {len(mountain.animals)} animals and {len(mountain.plants)} plants')
+        print(f'Mountain [{str(mountain.id)[:8]}] has {len(mountain.animals)} animals and {len(mountain.plants)} plants')
         for animal in mountain.animals:
             print(f'      {animal.species} [{animal.id}]')
         for plant in mountain.plants:
             print(f'      {plant.species} [{plant.id}]')
     for swamp in arboretum.swamps:
-        print(f'Swamp [{swamp.id}] has {len(swamp.animals)} animals and {len(swamp.plants)} plants')
+        print(f'Swamp [{str(swamp.id)[:8]}] has {len(swamp.animals)} animals and {len(swamp.plants)} plants')
         for animal in swamp.animals:
             print(f'      {animal.species} [{animal.id}]')
         for plant in swamp.plants:
